ae13a62aa727717aa69a3259662bd88a97f31d5f
philgrayphilgray <philgrayphilgray@Phillips-iMac.local>
2017-09-13 01:16:10 -0400

    add a sample q&a

--- pages/qa/index.vue
+++ pages/qa/index.vue
@@ -2,7 +2,8 @@
 main
   h1 Q&A
   article
-    h2 What are you doing?
+    h2 Describe your programming background in a few sentences.
+    p I’ve been self-studying web development with HTML, CSS, and JS for the past four years. I focused primarily on front-end design, and secondarily on learning the fundamentals of SPA development with popular JS frameworks. In recent months, however, my primary focus has shifted to really trying to learn and understand end-to-end JS and functional programming.
 
 </template>
 <style lang="scss" scoped>
