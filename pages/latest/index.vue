fb497938521cfd59ffa450a9f45e3ca4323c411f
philgrayphilgray <philgrayphilgray@Phillips-iMac.local>
2017-09-13 01:15:38 -0400

    fix vue templating syntax error

--- pages/latest/index.vue
+++ pages/latest/index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 main
   article
-    h2 {word}
+    h2 {{word}}
   article
     h2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero eligendi alias natus qui deleniti commodi officia dolores, possimus vel, magni aliquid ratione perspiciatis optio! Amet incidunt delectus commodi rerum consequuntur?
   
