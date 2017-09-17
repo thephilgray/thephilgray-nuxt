cd52a3f5a362dae9b42f5fdcef452fa85fa3935d
philgrayphilgray <philgrayphilgray@Phillips-iMac.local>
2017-09-16 22:45:12 -0400

    move global styles to main.css, add print style

--- layouts/default.vue
+++ layouts/default.vue
@@ -22,98 +22,9 @@ export default {
 </script>
 
 <style lang="scss">
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-}
-
-body {
-  font: 100%/1.6 "Lora", serif;
-  font-weight: 400; // margin: 0 auto;
-  padding: 2em;
-  text-align: left;
-  font-kerning: normal; // max-width: 45em;
-}
-
-p+p {
-  text-indent: 1em;
-}
-
-// modular scale
-h1 {
-  font-size: 2.488em;
-}
-
-h2 {
-  font-size: 2.074em;
-}
-
-h3 {
-  font-size: 1.728em;
-  margin: 0;
-  line-height: 1.4;
-}
-
-h4 {
-  font-size: 1.44em;
-  margin: 0;
-  line-height: 1.4;
-}
-
-h5 {
-  font-size: 1.2em;
-}
-
-h6 {
-  font-size: 1em;
-}
-
-h1 {
-  margin-bottom: 0;
-  letter-spacing: 0.1em;
-}
-
-span {
-  font-variant-numeric: oldstyle-nums;
-}
-
-ul {
-  margin: 0;
-  padding: 0;
-}
-
-li li {
-  margin-right: 1em;
-}
-
-.btn {
-  padding: 0.5em;
-  text-align: center;
-  cursor: pointer;
-  border: 1px solid #262427;
-  transition: all .5s;
-  &:hover {
-    background: #262427;
-    color: #fff;
-  }
-}
-
-.btn--full-width {
-  width: 100%;
-}
-
 .site>* {
   margin: 0 1em 1em;
 }
-
-
-
-
-
-
-
-
 
 
 
@@ -146,6 +57,12 @@ li li {
   }
   .site-footer {
     grid-area: footer;
+  }
+}
+
+@media only screen and (min-width:1200px) {
+  .site {
+    font-size: 120%;
   }
 }
 </style>
